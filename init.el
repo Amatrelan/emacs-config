e1682bbd6cb6a15b4dec748ce9008bc39faad8bf
amatrelan <github.afterglow462@passmail.net>
2024-08-02 11:10:38 +0300

    mix -> nix

--- init.el
+++ init.el
@@ -149,7 +149,7 @@
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       (mix +lsp) ; I hereby declare "nix geht mehr!"
+       (nix +lsp) ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +dragndrop +roam2 +present) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
